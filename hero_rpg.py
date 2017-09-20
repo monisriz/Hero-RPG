15bdbd70bc7e6f1d56a772855abb64acbffa1ec6
Monis Rizvi <monisriz@gmail.com>
2017-09-20 15:51:18 -0500

    Created a Character class that Hero/Goblin inherit from

--- hero_rpg.py
+++ hero_rpg.py
@@ -5,39 +5,33 @@
 # 2. do nothing - in which case the goblin will attack him anyway
 # 3. flee
 
-class Hero:
+class Character:
     def __init__(self, health, power):
         self.health = health
         self.power = power
 
+    def alive(self):
+        return self.health > 0
+
+class Hero(Character):
+
     def attack(self, enemy):
-            # Hero attacks Goblin
         enemy.health -= self.power
         print("You do {} damage to the goblin.".format(self.power))
         if enemy.health <= 0:
             print("The goblin is dead.")
 
-    def alive(self):
-        return self.health > 0
-
     def print_status(self):
         print("You have {} health and {} power.".format(self.health, self.power))
 
 
-class Goblin:
-    def __init__(self, health, power):
-        self.health = health
-        self.power = power
+class Goblin(Character):
 
     def attack(self, enemy):
-        # Goblin attacks Hero
         enemy.health -= self.power
         print("The goblin does {} damage to you.".format(self.power))
         if enemy.health <= 0:
             print("You are dead.")
-
-    def alive(self):
-        return self.health > 0
 
     def print_status(self):
         print("The goblin has {} health and {} power.".format(self.health, self.power))
